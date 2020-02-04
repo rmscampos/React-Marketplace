059fbd5a9f768201a4452b9a76b2f23f0bfa59f5
Regina Campos <rmscampos@gmail.com>
2020-02-04 16:55:07 -0600

    add user schema

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: 32
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    about: {
+        type: String,
+        trim: true,
+    },
+    salt: String,
+    role: {
+        // 0 is standard user, 1 is admin
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
+    }
+}, 
+{timestamps: true }
+);
