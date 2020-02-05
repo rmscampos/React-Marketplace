bdded9397dfa604b6b3e744333d19b018a9b783c
Regina Campos <rmscampos@gmail.com>
2020-02-05 15:59:03 -0600

    adds signin requirement and middleware

--- controllers/user.js
+++ controllers/user.js
@@ -52,3 +52,8 @@ exports.signout = (req, res) => {
     res.clearCookie('t');
     res.json({ message: 'Signout success' });
 };
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'auth'
+});
