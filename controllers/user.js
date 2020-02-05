baf18834d8e31590c87f094ab6cfda91c6853729
Regina Campos <rmscampos@gmail.com>
2020-02-05 15:45:07 -0600

    adds user signin auth and creates signed token

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const User = require ('../models/user');
 const {errorHandler} = require('../helpers/dbErrorHandler');
+const jwt = require('jsonwebtoken'); //to generate signed token
+const expressJwt = require('express-jwt'); //for authorization check
 
 exports.signup = (req, res) => {
     // console.log('req.body', req.body);
@@ -15,5 +17,33 @@ exports.signup = (req, res) => {
         res.json({
             user
         });
+    });
+};
+
+
+exports.signin = (req, res) => {
+    //find user based on email
+    const { email, password } = req.body
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                err: 'User with that email does not exist. Please signup'
+            });
+        }
+        // if user is found, make sure the email and pw match
+        // create authenticate method in user model
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            });
+        }
+
+        // generate a signed token with user id and secret
+        const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET)
+        // persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, {expire: new Date() + 9999})
+        // return response with user and token to frontend client
+        const {_id, name, email, role} = user
+        return res.json ({token, user: {_id, email, name, role}});
     });
 };
