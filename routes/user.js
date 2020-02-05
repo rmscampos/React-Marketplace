3bea607f16916d8c8d649193299a30ffcbf0ed65
Regina Campos <rmscampos@gmail.com>
2020-02-05 15:53:35 -0600

    adds signout method

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require('../validator');
 
 
 router.post('/signup', userSignupValidator, signup );
 router.post('/signin', signin );
+router.get('/signout', signout);
 
 module.exports = router;
