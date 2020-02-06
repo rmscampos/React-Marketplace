18196ccc0edc623f9ba825c00a63628f3cf1ec99
Regina Campos <rmscampos@gmail.com>
2020-02-06 17:05:38 -0600

    adds productId middleware

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,22 @@ const Product = require('../models/product');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: 'Product not found'
+            });
+        }
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product);
+};
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm()
@@ -13,22 +29,54 @@ exports.create = (req, res) => {
         if(err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
-            })
+            });
         }
-        let product = new Product(fields)
+        // check for all fields
+        const { 
+            name, 
+            description, 
+            price, 
+            category, 
+            quantity, 
+            shipping 
+        } = fields;
+
+        if (
+            !name || 
+            !description || 
+            !price || 
+            !category || 
+            !quantity || 
+            !shipping
+            ) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
+        let product = new Product(fields);
+
+        // 1kb = 1000
+        // 1mb = 1,000,000
 
         if(files.photo) {
-            product.photo.data = fs.readFileSync(files.photo.path)
-            product.photo.contentType = files.photo.type
+            // console.log('FILES PHOTO: ', files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1 MB in size'
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
         }
 
         product.save((err, result) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: errorHandler(err)
                 })
             }
             res.json(result);
-        })
-    })
+        });
+    });
 };
