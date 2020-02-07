b631c47543d0828d024d8069ffbbb408e4559f06
Regina Campos <rmscampos@gmail.com>
2020-02-07 12:36:29 -0600

    adds send photo method / works in postman

--- routes/product.js
+++ routes/product.js
@@ -11,7 +11,8 @@ const {
     list, 
     listRelated, 
     listCategories,
-    listBySearch
+    listBySearch,
+    photo
 } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
@@ -50,6 +51,7 @@ router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
 router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById);
 router.param('productId', productById);
