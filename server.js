1c41aac62f6ae4b83cac2200665f466357cd0afc
Regina Campos <rmscampos@gmail.com>
2020-02-07 14:20:14 -0600

    creates react app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 
 require('dotenv').config();
@@ -34,6 +35,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
@@ -41,7 +43,7 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
