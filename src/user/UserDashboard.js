5c8097ef73cc4ddeffe4c2e71e14ff49a62015a1
Regina Campos <rmscampos@gmail.com>
2020-02-08 23:47:22 -0600

    creates links on userdashboard and greeting

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,28 +1,90 @@
-import React from 'react';
-import Layout from '../core/Layout';
-import {isAuthenticated} from '../auth/index'
+import React, { useState, useEffect } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+import { getPurchaseHistory } from "./apiUser";
+import moment from "moment";
 
 const Dashboard = () => {
+    const [history, setHistory] = useState([]);
 
-    const {user: {_id, name, email, role}} = isAuthenticated();
+    const {
+        user: { _id, name, email, role }
+    } = isAuthenticated();
+    const token = isAuthenticated().token;
 
-    return (
-        <Layout title='Dashboard' description='User Dashboard' className='container'>
-            <div className ='card mb-5'>
-                <h3 className='card-header'>User Information</h3>
-                <ul className='list-group'>
-                    <li className='list-group-item'>{name}</li>
-                    <li className='list-group-item'>{email}</li>
-                    <li className='list-group-item'>{role === 1 ? 'Admin' : 'Registered User'}</li>
+    const init = (userId, token) => {
+        getPurchaseHistory(userId, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setHistory(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        init(_id, token);
+    }, []);
+
+    const userLinks = () => {
+        return (
+            <div className="card">
+                <h4 className="card-header">User Links</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/cart">
+                            My Cart
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link" to={`/profile/${_id}`}>
+                            Update Profile
+                        </Link>
+                    </li>
                 </ul>
             </div>
+        );
+    };
 
-            <div className='card mb-5'>
-                <h3 className='card-header'>Purchase History</h3>
-                <ul className='list-group'>
-                    <li className='list-group-item'>history</li>
-                    
+    const userInfo = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">User Information</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">{name}</li>
+                    <li className="list-group-item">{email}</li>
+                    <li className="list-group-item">
+                        {role === 1 ? "Admin" : "Registered User"}
+                    </li>
                 </ul>
+            </div>
+        );
+    };
+
+    const purchaseHistory = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">Purchase history</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">setHistory</li>
+                </ul>
+            </div>
+        );
+    };
+
+    return (
+        <Layout
+            title="Dashboard"
+            description={`Hello ${name}!`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-3">{userLinks()}</div>
+                <div className="col-9">
+                    {userInfo()}
+                    {purchaseHistory(history)}
+                </div>
             </div>
         </Layout>
     );
