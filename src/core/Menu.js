8e229dbe9778110d62994e6f51030ae329981352
Regina Campos <rmscampos@gmail.com>
2020-02-11 19:57:09 -0600

    deletes rogue semicolons

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -21,13 +21,13 @@ const Menu = ({ history }) => (
                 <li className='nav-item'>
                     <Link className='nav-link' style={isActive(history, '/user/dashboard')} to='/user/dashboard'>Dashboard</Link>
                 </li>
-            )};
+            )}
 
             {isAuthenticated() && isAuthenticated().user.role === 1 && (
                 <li className='nav-item'>
                     <Link className='nav-link' style={isActive(history, '/admin/dashboard')} to='/admin/dashboard'>Admin Dashboard</Link>
                 </li>
-            )};
+            )}
 
             {!isAuthenticated() && (
                 <Fragment>
@@ -38,12 +38,12 @@ const Menu = ({ history }) => (
                         <Link className='nav-link' style={isActive(history, '/signup')} to='/signup'>Sign Up</Link>
                     </li>
                 </Fragment>
-            )};
+            )}
 
             {isAuthenticated() && (
                 <li className='nav-item'>
                     <span className='nav-link' style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(() => {
-                    history.push('/');
+                    history.push('/')
                 })}>
                         Sign Out
                     </span>
